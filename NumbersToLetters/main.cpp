15afdf806fdc4df19116d048e0fae92240fee688
magrcruz <50184830+magrcruz@users.noreply.github.com>
2020-03-13 20:23:28 -0500

    Update main.cpp

--- NumbersToLetters/main.cpp
+++ NumbersToLetters/main.cpp
@@ -2,66 +2,68 @@
 #include <string>
 using namespace std;
 
-//la idea es separar de tres en tres
-void imprimir3Cifras(int x){
-    string cero_nine[]={"cero","un","dos","tres","cuatro","cinco","seis","siete","ocho","nueve"};
-    int a=x/100;
-    if(a){
-        if (a==5) cout<<"quinientos ";
-        else if(a==7) cout<<"setecientos ";
-        else if (a==9) cout<<"novecientos ";
-        else if (a==1){
-            if (x%100) cout<<"ciento ";
-            else{
-                cout<<"cien ";
-                return;
-            }
-        }
-        else cout<<cero_nine[a]<<"cientos ";
-    }
-    a=(x-a*100)/10;
-    if(a){
-        string decenas[]={"","diez","veinte","treinta ","cuarenta ","cincuenta ","sesenta ","setenta ","ochenta ","noventa "};
-        if (a==2){
-            if (x%10){
-                cout<<"veinti"<<cero_nine[x%10];
-                return;
-            }
-            else{
-                cout<<"veinte ";
-                return;
-            }
-        }
-        else if(a==1){
-            string ten_fiveteen[]={ "diez","once","doce","trece","catorce","quince"};
-            if ((a%10)<6) cout<<ten_fiveteen[x%10];
-            else cout<<"dieci"<<cero_nine[x%10];
-            return;
-        }
-        else{
-            cout<<decenas[a];
-            if (x%10) cout<<"y ";
-        }
-    }
-    a=x%10;
-    if (a) cout<<cero_nine[a];
-}
+void imprimir3Cifras(int x);
+void imprimirNumeroLetras(long long numero);
 
 int main()
 {
     long long numero;
-    long long base=1000000000;
+
     cout<<"Ingrese numero: ";
     cin>>numero;
+    cout<<"El numero se lee: ";
+    imprimirNumeroLetras(numero);
+    return 0;
+}
 
-    string denominaciones[]={" mil "," millones "," mil ",""};
+void imprimir3Cifras(int x){
+    //CENTENAS
+    int a=x/100;
+    if(a){//verificar primero casos especiales
+        string centenas[]={"","dos","tres","cuatro","quini",
+            "seis","sete","ocho","nove"};
+        if (a==1&&!(x%100)){
+            cout<<"cien ";
+            return;
+        }
+        else cout<<centenas[a-1]<<(a!=5?"ci":"")<<"ento"<<(a!=1?"s ":" ");
+    }
+    //DECENAS
+    string digitos[]={"","un","dos","tres","cuatro","cinco",
+            "seis","siete","ocho","nueve"};
+    a=(x-a*100)/10;
+    if(a){
+        string decenas[]={"","dieci","veinti","treinta ","cuarenta ",
+                "cincuenta ","sesenta ","setenta ","ochenta ","noventa "};
+        if (a==2&&!(x%10)){
+            cout<<"veinte ";
+            return;
+        }
+        else if(a==1&&((x%10)<6)){
+            string ten_fiveteen[]={ "diez","once","doce","trece","catorce","quince"};
+            cout<<ten_fiveteen[x%10];
+            return;
+        }
+        else{
+            cout<<decenas[a];
+            if ((x%10)&&(a!=2)&&(a!=1)) cout<<"y ";
+        }
+    }
+    //UNIDADES
+    a=x%10;
+    if (a) cout<<digitos[a];
+}
+
+void imprimirNumeroLetras(long long numero){
     if (numero==0){
         cout<<"cero";
-        return 0;
+        return;
     }
+    long long base=1000000000;
+    string denominaciones[]={" mil "," millones "," mil ",""};
     int aux=0;
     for (int i=0;i<4;i++,base/=1000){
-        aux=numero/base;
+        aux=numero/base;//descomponer el numero de tres en tres
         numero-=(aux*base);
         if(aux!=1) imprimir3Cifras(aux);
         else if(i==1){
@@ -70,8 +72,6 @@ int main()
         }
         else if(i==3) cout<<"un";
         if(aux) cout<<denominaciones[i];
-        if(i==3&&aux%10==1) cout<<"o";
+        if(i==3&&aux%10==1&&aux%100!=11) cout<<"o";
     }
-
-    return 0;
 }
